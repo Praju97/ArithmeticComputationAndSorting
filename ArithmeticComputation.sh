01a248ea61fb19d619483b0175b472f0e14d8506
Salina <amargunjal28@gmail.com>
2020-01-30 15:15:34 +0530

    ADDED:Storing Results Using Dictionary

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
+
+declare -A resultDictionary
+
 echo "Welcome To Arithmetic Computation And Sorting"
  read -p "Enter value of a: " a
  read -p "Enter value of b: " b
@@ -8,4 +11,9 @@ result1=$(($a+$b*$c))
 result2=$(($a*$b+$c))
 result3=$(($c+$a/$b))
 result4=$(($a%$b+$c))
+
+resultDictionary[result1]=$result1
+resultDictionary[result2]=$result2
+resultDictionary[result3]=$result3
+resultDictionary[result4]=$result4
 
