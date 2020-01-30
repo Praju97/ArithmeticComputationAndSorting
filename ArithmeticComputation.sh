da87719b7af71cbb92a2b980c4581adb8eb1fb7a
Salina <amargunjal28@gmail.com>
2020-01-30 16:00:25 +0530

    ADDED:Result In Ascending Order

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -39,3 +39,21 @@ do
 done
 
 echo ${array[@]}
+
+
+
+
+for (( i=0; i<$((${#array[@]}-1)); i++ ))
+do
+  for (( j=i+1; j<${#array[@]}; j++ ))
+  do
+    if ((${array[i]%.*}>${array[j]%.*}))
+	 then
+		temp=${array[i]}
+      array[i]=${array[j]}
+		array[j]=$temp
+     fi
+	done
+done
+
+echo ${array[@]}
