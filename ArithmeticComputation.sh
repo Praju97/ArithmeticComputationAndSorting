0b53be2c9288155cb1eb37a062d465de219d3e66
Salina <amargunjal28@gmail.com>
2020-01-30 14:48:04 +0530

    ADDED:Computing First Operation

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome To Arithmetic Computation And Sorting"
  read -p "Enter value of a: " a
  read -p "Enter value of b: " b
  read -p "Enter value of c: " c
 
+result1=$(($a+$b*$c))
